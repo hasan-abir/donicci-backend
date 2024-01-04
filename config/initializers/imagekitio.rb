beee9bac2ee879ff8fa14d581b6dd7540121d36e
Hasan Abir <contact.hasanabir@gmail.com>
2024-01-04 09:24:10 +0600

    Imagekit config for prod

--- config/initializers/imagekitio.rb
+++ config/initializers/imagekitio.rb
@@ -1,7 +1,5 @@
 ImageKitIo.configure do |config|
-  if Rails.env.development? || Rails.env.test?
     config.public_key = ENV["IMAGEKIT_PUBLIC_KEY"]
     config.private_key = ENV["IMAGEKIT_PRIVATE_KEY"]
     config.url_endpoint = ENV["IMAGEKIT_URL_ENDPOINT"]
-  end
 end
